9e72dfe52f5412aa6927728f2304e69683e56836
L-Spiro <yogurtemperor@gmail.com>
2025-12-28 21:00:02 +0900

    Better’d icon organization.

--- BuildResources/BuildIcons.bat
+++ BuildResources/BuildIcons.bat
@@ -1,5 +1,5 @@
-SurfaceLevel2.exe -file "..\Resources\icons8-nest-48.png" -out_file "..\Resources\icons8-nest-48.ico" -fit 48 48 -icon -filter cardinal
-SurfaceLevel2.exe -file "..\Resources\icons8-nest-32.png" -out_file "..\Resources\icons8-nest-32.ico" -fit 32 32 -icon -filter cardinal
+SurfaceLevel2.exe -file "icons8-nest-48.png" -out_file "..\Resources\icons8-nest-48.ico" -fit 48 48 -icon -filter cardinal
+SurfaceLevel2.exe -file "icons8-nest-32.png" -out_file "..\Resources\icons8-nest-32.ico" -fit 32 32 -icon -filter cardinal
 
 @rem SurfaceLevel2.exe -file "..\Resources\icons8-nest-60.png" -out_file "..\Resources\icons8-nest-48 robidouxsharp.ico" -fit 48 48 -icon -filter robidouxsharp
 @rem SurfaceLevel2.exe -file "..\Resources\icons8-nest-60.png" -out_file "..\Resources\icons8-nest-48 adobebicubic.ico" -fit 48 48 -icon -filter adobebicubic
